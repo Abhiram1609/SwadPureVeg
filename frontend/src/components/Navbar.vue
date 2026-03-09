db153bbf94342b4277e194ea6b8e7b72d6e80299
Abhiram <abhirampatankar34916@gmail.com>
2026-03-09 13:50:59 +0530

    Navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      Swad Pure Veg
+    </div>
+
+    <ul class="nav-links">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/menu">Menu</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.navbar{
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  padding:15px 40px;
+  background:#e45604;
+  color:white;
+  width: 100%;
+  height: 6rem;
+}
+
+.logo{
+  font-size:22px;
+  font-weight:bold;
+}
+
+.nav-links{
+  display:flex;
+  list-style:none;
+  gap:25px;
+}
+
+.nav-links a{
+  text-decoration:none;
+  color:white;
+  font-weight: 500;
+  font-size: large;
+
+}
+
+
+/* active page underline */
+.nav-links a.router-link-active{
+  border-bottom:2px solid #ffffff;
+}
+
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,112 @@
+<script setup>
+</script>
+
+<template>
+  <div class="home">
+
+    <!-- HERO SECTION -->
+    <section class="hero">
+      <h1>Swad Pure Veg</h1>
+      <p>Delicious Pure Vegetarian Food</p>
+
+      <RouterLink to="/menu">
+        <button class="menu-btn">View Menu</button>
+      </RouterLink>
+    </section>
+
+    <!-- CATEGORIES -->
+    <section class="categories">
+      <h2>Our Categories</h2>
+
+      <div class="category-container">
+
+        <div class="category-card">
+          🍔
+          <h3>Burgers</h3>
+        </div>
+
+        <div class="category-card">
+          🌮
+          <h3>Chaat</h3>
+        </div>
+
+        <div class="category-card">
+          🍜
+          <h3>Chinese</h3>
+        </div>
+
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<style scoped>
+
+.home{
+  text-align:center;
+}
+
+/* HERO */
+
+.hero{
+  padding:80px 20px;
+  background:#f8fafc;
+}
+
+.hero h1{
+  font-size:48px;
+  margin-bottom:10px;
+}
+
+.hero p{
+  font-size:18px;
+  margin-bottom:20px;
+}
+
+.menu-btn{
+  padding:12px 25px;
+  font-size:16px;
+  border:none;
+  background:#f97316;
+  color:white;
+  cursor:pointer;
+  border-radius:6px;
+}
+
+.menu-btn:hover{
+  background:#ea580c;
+}
+
+/* CATEGORIES */
+
+.categories{
+  padding:60px 20px;
+}
+
+.category-container{
+  display:flex;
+  justify-content:center;
+  gap:30px;
+  margin-top:30px;
+}
+
+.category-card{
+  width:150px;
+  height:120px;
+  background:white;
+  border-radius:10px;
+  box-shadow:0 3px 10px rgba(0,0,0,0.1);
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  font-size:30px;
+}
+
+.category-card h3{
+  margin-top:10px;
+  font-size:16px;
+}
+
+</style>
